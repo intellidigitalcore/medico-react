298978cf1842a2c60512d7f629e06375f89cedb1
najmul-eimon <najmul.eimon@intelli.global>
2023-02-01 10:09:52 +0600

    scroll to top added

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,9 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/navigation/Header';
 import Footer from '../components/navigation/Footer';
 import Loader from '../components/shared/Loader';
+import {BsArrowUp} from 'react-icons/bs';
 
 const Layout = () => {
   const [loading, setLoading] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScrollToTop = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
 
   useEffect(() => {
 
@@ -15,14 +21,26 @@ const Layout = () => {
   
     const body = document.querySelector('body');
     loading ? body.style.overflow = "hidden" : body.style.overflow = "visible";
-  }, [loading])
+  }, [loading]);
+
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false);
+    }
+
+    window.addEventListener('scroll', handleScrollButtonVisibility);
+    return () => {
+      window.removeEventListener('scroll', handleScrollButtonVisibility);
+    }
+  }, []);
 
   return (
     <>
       {loading && <Loader/>}
       <Header/>
-      <Outlet/>
+        <Outlet/>
       <Footer/>
+      <button onClick={handleScrollToTop} type='button' className={`${showButton ? 'show' : ''} scrollTopButton`}><BsArrowUp/></button>
     </>
   )
 }
